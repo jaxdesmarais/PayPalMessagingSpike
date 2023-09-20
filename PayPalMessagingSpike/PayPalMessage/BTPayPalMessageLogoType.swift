84d03394e1c96513c4d2dd69d070cf029b6504db
= <=>
2023-09-20 16:24:49 -0700

    Wrapped MessageLogo
    /// Text alignment option for a PayPal Message
    @objc public enum BTPayPalMessageLogoType: Int {
    
        /// PayPal logo positioned inline within the message
        case inline
        /// Primary logo including both the PayPal monogram and logo
        case primary
        /// Alternative logo including just the PayPal monogram
        case alternative
        /// "PayPal" as bold text inline with the message
        case none
    
        var offerTypeRawValue: PayPalMessageLogoType? {
            switch self {
            case .inline:
                return .inline
            case .primary:
                return .primary
            case .alternative:
                return .alternative
            case .none:
                return PayPalMessageLogoType.none
            }
        }
    }

---
+++ PayPalMessagingSpike/PayPalMessage/BTPayPalMessageLogoType.swift
@@ -0,0 +1,35 @@
+//
+//  BTPayPalMessageLogoType.swift
+//  PayPalMessagingSpike
+//
+//  Created by Justin Warmkessel on 9/20/23.
+//
+
+import Foundation
+import PayPalMessages
+
+/// Text alignment option for a PayPal Message
+@objc public enum BTPayPalMessageLogoType: Int {
+
+    /// PayPal logo positioned inline within the message
+    case inline
+    /// Primary logo including both the PayPal monogram and logo
+    case primary
+    /// Alternative logo including just the PayPal monogram
+    case alternative
+    /// "PayPal" as bold text inline with the message
+    case none
+
+    var offerTypeRawValue: PayPalMessageLogoType? {
+        switch self {
+        case .inline:
+            return .inline
+        case .primary:
+            return .primary
+        case .alternative:
+            return .alternative
+        case .none:
+            return PayPalMessageLogoType.none
+        }
+    }
+}
