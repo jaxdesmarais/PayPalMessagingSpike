59c9006dc358cf594e9b6dea1c920d7719587452
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-09-20 09:33:14 -0500

    remove unneeded comments

--- PayPalMessagingSpike/ViewController.swift
+++ PayPalMessagingSpike/ViewController.swift
@@ -6,31 +6,8 @@ class ViewController: UIViewController, PayPalMessageViewEventDelegate, PayPalMe
 
     var payPalMessageView: BTPayPalCreditMessageView?
 
-    // outstanding questions
-    // - do we want to use the client/request pattern? it doesn't really make a ton of sense with just UI?
-    // - what do we need to wrap or not wrap for v1? everything? only some classes?
-    // - class name: PayPalCreditMessage____
-
-    // thoughts
-    // - merchants should pass in APIClient and we should obfuscate clientID from them completely
-    // - everything in PayPalMessageData and PayPalMessageStyle should be wrapped in a request vs separate objects
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        let messageConfig = PayPalMessageConfig(
-//            data: PayPalMessageData(
-//                clientID: "ASPBQAggBcUvZJ0kFFBizjYapdjokGMcAzBFoC0xIAYY-4iuJH3NxAgkdUEyQ6oCPQiKNRZaWUogS0d6",
-//                amount: 2.0,
-//                offerType: .payLaterLongTerm,
-//                environment: .sandbox
-//            ),
-//            style: PayPalMessageStyle(
-//                logoType: .primary,
-//                color: .black,
-//                textAlignment: .center
-//            )
-//        )
 
         let apiClient = BTAPIClient(authorization: "sandbox_9dbg82cq_dcpspy2brwdjr3qn")!
         let request = BTPayPalCreditMessageRequest()
